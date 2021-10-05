3249566c017c30ebbe22e12c20049af9fed0a7d9
Vadim Chistyakov <titaniys007@rambler.ru>
2021-10-05 16:29:53 +0300

    house robber medium

---
+++ LeetCodeExamples/Medium/DynamicProgramming/HouseRobber.swift
@@ -0,0 +1,21 @@
+//
+//  HouseRobber.swift
+//  LeetCodeExamples
+//
+//  Created by Vadim Chistiakov on 05.10.2021.
+//
+
+import Foundation
+
+func rob(_ nums: [Int]) -> Int {
+    if nums.count == 1 { return nums[0] }
+    var dp = [Int]()
+    dp.append(0)
+    dp.append(nums[0])
+    
+    for index in 1..<nums.count {
+        let current = nums[index]
+        dp.append(max(dp[index-1] + current, dp[index]))
+    }
+    return dp[nums.count]
+}
