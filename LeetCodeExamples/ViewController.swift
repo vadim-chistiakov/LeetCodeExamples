e31981c2bb5f577c7caf992d0f7542b834196698
Vadim Chistyakov <titaniys007@rambler.ru>
2022-03-21 12:09:29 +0300

    3 sum

--- LeetCodeExamples/ViewController.swift
+++ LeetCodeExamples/ViewController.swift
@@ -12,22 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // [1,0,x,y,0,1] -> 2
-        // [1,0,0,0,0,0,0] -> 6
-        // 1минимум - свободное
-        // 1мин - занятое
-        // 1 [0,1,0]
-        // [1,1,1,1,1,0] -> 1
-
-        // [1,0,0,0,0,1,0,0,0] -> 3
-        // [0,0,0,1,0,0,0,0,1] -> 3
-        
-        print(findEmptySeat([1,0,0,0,0,1]))
-        print(findEmptySeat([1,0,0,0,0,0,0]))
-        print(findEmptySeat([0,1,0]))
-        print(findEmptySeat([1,1,1,1,1,0]))
-        print(findEmptySeat([1,0,0,0,0,1,0,0,0]))
-        print(findEmptySeat([0,0,0,1,0,0,0,0,1]))
+        print(threeSum([-5,0,-2,3,-2,1,1,3,0,-5,3,3,0,-1]))
 
         
     }
